0fca705e45040059e6582ab2cb86966354f82a2d
JonasDavidTR <jonasdavidcosta@gmail.com>
2023-12-14 00:10:53 -0300

    commit update 1.0

--- projeto/src/functions/functions.py
+++
@@ -1,7 +0,0 @@
-
-def processar_dados(googleUser):
-    userProfile = googleUser.getBasicProfile()
-    userId = ('ID: ' + userProfile.getId())
-    userNome = ('Name: ' + userProfile.getName())
-    userImage = ('Image URL: ' + userProfile.getImageUrl())
-    userEmail = ('Email: ' + userProfile.getEmail())
